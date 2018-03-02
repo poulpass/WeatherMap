6454e0118782786c5faadb4c46ffdb0687810288
Pierre-Loïc <33351109+poulpass@users.noreply.github.com>
2018-03-02 12:11:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+//utilise moment.js pour afficher la date au format "JJ MMMM AAAA"
 moment.locale("fr");
 $("#date").text( moment().format('LL'));  
 
 
-
+//vas chercher les données rentrées par l'utilisateur dans l'input
 var ville = $("#address").val();
 
+//utilise l'api Open Weather Map pour inscrire dans les données météo dans la page
 function meteo(ville){
 	$.ajax({
 		url: "https://api.openweathermap.org/data/2.5/weather?q="+ ville +"&lang=fr&units=metric&appid=3c52f05dd1f0d3ad4faba99820954ae9",
 		type: "GET",
-		dataType: "jsonp",
+		dataType: "json",
 		success: function(data){
 			 $("#tempMax").text("Temp. Max "+ data.main.temp_max+"°");
 			 $("#tempMin").text("Temp. Min "+ data.main.temp_min+"°");
@@ -18,20 +20,22 @@ function meteo(ville){
 			 $("#humidity").text("Humidité "+ data.main.humidity+" %");
 			 $("#temp").text(parseInt(data.main.temp)+"°");
 			 $("#coordonées").text("Longitude = " + data.coord.lon+ " Latitude = " + data.coord.lat )
+//Utilise l'iframe google map pour afficher la carte et se sert de la variable "ville" pour se positioner sur l'endroi souhaité
 			 $("#map").html("<iframe src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBMtdsVDGb8x9NJku3jdI3eFfAL9tu22ao&q="+ville+"&zoom=12&maptype=roadmap' width='100%' height='100%' frameborder='0'></iframe>");
 
 		}
 	})
 
 }
-
+// lance la fonction meteo au demarrage 
 $(document).ready(function(){
 	meteo(ville);
-
+// lance la fonction au clic du bouton "valider"
 	$("#submit").click(function(){
 		ville = $("#address").val();
 		meteo(ville);
 	})
+//lance la fonction quand on tape entrer
 	$("#address").keypress(function(e){
 		if ( e.keyCode==13 ){
 			ville = $("#address").val();
@@ -39,109 +43,3 @@ $(document).ready(function(){
 		}
 	});
 })
-
-
-/*
-$("#submit").click(function(){
-	var ville = $("#address").val();
-	console.log(ville);
-	varFetch = "http://api.openweathermap.org/data/2.5/weather?q="+ville+"&lang=fr&units=metric&appid=3c52f05dd1f0d3ad4faba99820954ae9";
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#tempMax").text("Température Max "+ resJson.main.temp_max+"°"))
-
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#tempMin").text("Température Min "+ resJson.main.temp_min+"°"))
-
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#pressure").text("Pression atmospherique "+ resJson.main.pressure+" Pa"))
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#windspeed").text("Vitesse de vent "+ resJson.wind.speed+" Km/h"))
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#humidity").text("Humidité "+ resJson.main.humidity+" %"))
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#temp").text(parseInt(resJson.main.temp)+"°"))
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#coordonées").text("Longitude = " + resJson.coord.lon+ " Latitude = " + resJson.coord.lat ))
-
-
-
-});
-
-
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#tempMax").text("Température Max "+ resJson.main.temp_max+"°"))
-
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#tempMin").text("Température Min "+ resJson.main.temp_min+"°"))
-
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#pressure").text("Pression atmospherique "+ resJson.main.pressure+" Pa"))
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#windspeed").text("Vitesse de vent "+ resJson.wind.speed+" Km/h"))
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#humidity").text("Humidité "+ resJson.main.humidity+" %"))
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#temp").text(parseInt(resJson.main.temp)+"°"))
-
-window.fetch(varFetch)
-.then(res => res.json())
-.then(resJson => $("#coordonées").text("Longitude = " + resJson.coord.lon + " Latitude = " + resJson.coord.lat ))
-
-
-
-
-
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: {lat: 43.11, lng: 1.61}
-        });
-        var geocoder = new google.maps.Geocoder();
-
-        document.getElementById('submit').addEventListener('click', function() {
-          geocodeAddress(geocoder, map);
-        });
-      }
-
-      function geocodeAddress(geocoder, resultsMap) {
-        var address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            
-
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }
-
-*/
-
-//$("#map").html("<iframe src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBMtdsVDGb8x9NJku3jdI3eFfAL9tu22ao&q="+ville+"&zoom=12&maptype=roadmap' width='100%' height='100%' frameborder='0'></iframe>");
